92ab319ec42c471ec0ccbff475c67b5afab2d712
Onda <75018767+ondastudio@users.noreply.github.com>
2023-07-15 13:00:32 +0100

    Update team-slider.js

--- team-slider.js
+++ team-slider.js
@@ -8,20 +8,26 @@ document.addEventListener("DOMContentLoaded", () => {
     slidesPerView: "auto",
     keyboard: true,
     navigation: {
-      nextEl: ".swiper-arrow.is-next",
-      prevEl: ".swiper-arrow.is-prev"
+      nextEl: ".swiper-arrow.is-testimonial.is-next",
+      prevEl: ".swiper-arrow.is-testimonial.is-prev"
     }
   });
 
   if (window.innerWidth < 992) {
     const teamSwiper = new Swiper(".swiper.is-team", {
       speed: 600,
-      spaceBetween: 40,
+      spaceBetween: 20,
       direction: "horizontal",
       slideActiveClass: "is-active",
       slideToClickedSlide: true,
       slidesPerView: "auto",
-      keyboard: true
+      centeredSlides: true,
+      initialSlide: 1,
+      keyboard: true,
+      navigation: {
+        nextEl: ".swiper-arrow.is-team.is-next",
+        prevEl: ".swiper-arrow.is-team.is-prev"
+      }
     });
   }
 });
